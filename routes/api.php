f98d4d8410dfd1d058c5d5b47abbbefab2235ccc
fontre <447488560@qq.com>
2019-02-26 22:30:05 +0800

    test

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return 1212;
+    return $request->user();
+});
+
+
+// 登录接口
+Route::get('test', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return 2;
+    // return Article::all();
+});
+
+// 登录接口
+Route::post('login', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return 2;
+    // return Article::all();
+});
