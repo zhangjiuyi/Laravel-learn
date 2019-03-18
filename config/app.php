a84ad4cdbc396fde5758cf43158c02e7bc57228a
Jiuyi <447488560@qq.com>
2019-03-19 00:06:32 +0800

    设置时区

--- config/app.php
+++ config/app.php
@@ -66,8 +66,8 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
-    'timezone' => 'UTC',
+        // 设置时区
+    'timezone' => 'Asia/Shanghai',
 
     /*
     |--------------------------------------------------------------------------
